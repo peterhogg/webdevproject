476f8b07a39373c3df2ce2d1109b37f5d99e0c6c
Peter Hoggarth <hoggarth.peter@gmail.com>
2018-08-01 18:07:59 -0400

    Added simple user route

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Register models
+var User = require("./models/User");
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find({})
+    .then(users => {
+      res.send(users);
+    })
 });
+
+router.post("/", function(req,res){
+  user = new User({
+    "name" : req.body.name,
+    "email": req.body.email
+  });
+  user.save()
+  .then((user) => {
+    res.json(user).status(200);
+  })
+  .catch((error)=>{
+    console.log(error);
+    res.send(error).status(500);
+  })
+})
 
 module.exports = router;
