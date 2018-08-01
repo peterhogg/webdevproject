9b643084be61cec5892625a95752807bd941bc9c
Peter Hoggarth <hoggarth.peter@gmail.com>
2018-08-01 18:00:03 -0400

    Added basic mongoose configuration

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/webdevproject');
+
 var app = express();
 
 // view engine setup
